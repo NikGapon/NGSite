a87c790a2bd25394e6d8927cfc0a8fa1feaa4eb2
Nikola <nik.gapon84@gmail.com>
2025-01-10 17:26:53 +0400

    init

---
+++ src/main/java/ru/nikgapon/ngsite/NgSiteApplication.java
@@ -0,0 +1,13 @@
+package ru.nikgapon.ngsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nikgapon/ngsite/NgSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikgapon.ngsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
