b924192191cecb76221f8b552d5dca90c430dde4
NikGapon <nik.gapon84@gmail.com>
2025-03-21 00:07:16 +0400

    aboba

---
+++ src/main/java/ru/nikgapon/ngsite/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package ru.nikgapon.ngsite.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/swagger-ui/**");
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                authorizeRequests -> authorizeRequests.requestMatchers("/swagger-ui/**")
+                        .permitAll());
+
+        return http.build();
+    }
+}
